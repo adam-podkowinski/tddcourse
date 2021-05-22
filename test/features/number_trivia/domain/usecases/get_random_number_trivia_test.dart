b93010a0ca642b152ea432cc300fcca3182ba622
Adam Podkowinski <adampodkdev@gmail.com>
2021-05-22 23:10:40 +0200

    Create get random number trivia test

--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,37 +1,37 @@
 import 'package:dartz/dartz.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:tddcource/core/usecases/usecase.dart';
 import 'package:tddcource/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:tddcource/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tddcource/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:tddcource/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 
-import 'get_concrete_number_trivia_test.mocks.dart';
+import 'get_random_number_trivia_test.mocks.dart';
 
 @GenerateMocks([NumberTriviaRepository])
 void main() {
-  GetConcreteNumberTrivia? usecase;
+  GetRandomNumberTrivia? usecase;
   MockNumberTriviaRepository? mockNumberTriviaRepository;
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository!);
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository!);
   });
 
-  final tNumber = 1;
-  final tNumberTrivia = NumberTrivia(number: tNumber, text: 'test');
+  final tNumberTrivia = NumberTrivia(number: 1, text: 'test');
 
   test(
-    'should get trivia for the number from the repository',
+    'should get trivia from the repository',
     () async {
       //arrange
-      when(mockNumberTriviaRepository!.getConcreteNumberTrivia(any))
+      when(mockNumberTriviaRepository!.getRandomNumberTrivia())
           .thenAnswer((_) async => Right(tNumberTrivia));
       ////act
-      final result = await usecase!(Params(number: tNumber));
+      final result = await usecase!(NoParams());
       //assert
       expect(result, Right(tNumberTrivia));
-      verify(mockNumberTriviaRepository!.getConcreteNumberTrivia(tNumber));
+      verify(mockNumberTriviaRepository!.getRandomNumberTrivia());
       verifyNoMoreInteractions(mockNumberTriviaRepository);
     },
   );
