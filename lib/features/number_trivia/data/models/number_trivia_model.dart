a5dcb03fd5611fc2c6b42a8735818ffeb9a6c4a9
adampodkdev@gmail.com <adampodkdev@gmail.com>
2021-06-27 14:47:19 +0200

    Add number_trivia_model.dart and it's test

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,6 @@
+import 'package:tddcource/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({required text, required number})
+      : super(text: text, number: number);
+}
